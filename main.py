d71690d49ddc2739bf7e1aa640d005188cf56fc1
Jwnwnj <149147034+Jwnwnj@users.noreply.github.com>
2024-10-04 17:25:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def make_request(url, headers, cookies):
 def index():
     if request.method == 'POST':
         password = request.form['password']
-        if password == "HENRY 786":
+        if password == "H3NRY_INX1D3":
             return redirect(url_for('dashboard'))
         else:
             return render_template('index.html', error="Incorrect Password! Try again.")
